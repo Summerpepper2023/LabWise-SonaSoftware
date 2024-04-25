cbd44e95891c0364fc117f82068c68911851bfe9
Summerpepper2023 <125401240+Summerpepper2023@users.noreply.github.com>
2024-04-25 11:54:36 -0500

    Update helpers.py
    Update User Class

--- helpers.py
+++ helpers.py
@@ -1,82 +1,75 @@
 class User:
-    def __init__(self, user_id:int, name:str,  username:str, password:str, job:str):
-        self.id = user_id
-        self.username = username
+    def __init__(self, id:int, name:str, username:str, password:str, job:str):
+        self.id = id
         self.name = name
+        self.username = username
         self.password = password
         self.job = job
-    
+        
     def __str__(self):
-        return f"Name:{self.name}, id:{self.id}, username:{self.username}, password:{self.password}, job:{self.job}"
+        return (f"Id:{self.id}\nname:{self.name}\nusername:{self.username}\npassword:{self.password}\njob:{self.job}")
     
-    def enter_db(self):
-        # Ingresamos al usuario  la base de datos de usuarios
-        return
-    
-    def update_db(self):
-        # Actualizamos la base dfe datos de usuarios
-        return
-    
-    def delet_db(self):
-        # Eliminamos el usuario de la base de datos de usuarios
-        return
+    @classmethod
+    def get(cls):
+        id = int(input("id: "))
+        name = input("Name: ")
+        username = input("username: ")
+        password = input("Password: ")
+        job = input("Job: ")
+        return cls(id, name, username, password, job)
 
-class Product:
-    def __init__(self, product_id:int, name:str, quantity:str, temperature:float, batch:str, processes:list, presentation:str, inCharge:str):
-        self.id = product_id
-        self.name = name
-        self.quantity = quantity
-        self.temperature = temperature
-        self.batch = batch
-        self.processes = processes
-        self.presentation = presentation
-        self.inCharge = inCharge
+    @property
+    def id(self):
+        return self._id
+    @id.setter
+    def id(self, id):
+        if not id:
+            raise ValueError("Invalid Id")
+        self._id = id
     
-    def __str__(self):
-        return f"id: {self.id}, name:{self.name}, quantitiy:{self.quantity}, temperature:{self.temperature}, 
-                batch:{self.batch}, process:{self.process}, presentatio:{self.presentation}, in charge:{self.inCharge}"
+    @property
+    def name(self):
+        return self._name
+    @name.setter
+    def name(self, name):
+        if not name:
+            raise ValueError("Missing name")
+        self._name = name
+    
+    @property
+    def username(self):
+        return self._username
+    @username.setter
+    def username(self,username):
+        if not username:
+            raise ValueError("Missing username")
+        self._username = username
+        
+    @property
+    def password(self):
+        return self._password
+    @password.setter
+    def password(self,password):
+        if not password:
+            raise ValueError ("Missing password")
+        self._password=password
 
-    def enter_db(self):
-        # Ingresamos el usuario a la base de datos de usuarios
+    @property
+    def job(self):
+        return self._job
+    @job.setter
+    def job(self, job):
+        jobs = ["Admin", "Analist", "BPM"]
+        if not job or job not in jobs:
+            raise ValueError("Invalid Job")
+        self._job = job
+    
+    def Insert_db(self, db):
+        # Insertar nuevo usuario a la base de datos
         return
-    
-    def update_db(self):
-    # Actualizamos la informacion de la base de datos de usuarios
+    def Delete_db(self, db):
+        # Eliminamos al usuario de la base de datos
         return
-
-    
-    def delete_db(self):
-        # Eliminamos el producto de la base de datos de usuarios
-        return
-
-
-class Certificate:
-    def __init__(self, certificate_id:int, process:str, date:str, product_info:dict, inCharge:str):
-        self.id = certificate_id
-        self.process = process
-        self.date = date
-        self.product_info = product_info
-        self.inCharge = inCharge
-        self.approved = False
-        self.pdf = None
-        self.signature = None
-    
-    def __str__(self):
-        return f"id:{self.id}, process:{self.process}, date:{self.date}, product info:{self.product_info}, in charge:{self.inCharge}"
-
-    def generate_pdf(self, template):
-        # Se genera el pdf del certificado basandose en un template y se añade a la base de datos, 
-        # si ya una anterior version de este se elimina
-        return 
-    
-    def aprove_certificate(self, pdf, signature):
-        # Se incluye la firma del administrador en el archivo del certificado
-        return
-    
-    def send_BPM(self, pdf):
-        # Ingresamos el certificado a la base de datos del area de BPM
-        return
-    
-    def return_certificate(self, pdf):
-        # Cambiamos el estado de aprovado a False y se elimina de la base de BPM
+    def Update_db(self, db):
+        # Actualiza la información del usuario en la base de datos
         return
